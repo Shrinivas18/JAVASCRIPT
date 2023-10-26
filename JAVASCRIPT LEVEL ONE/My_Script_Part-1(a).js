8cf417ec2f0c7cfb5e1aff73a58560232f786a48
Shrinivas Jamvant <119414094+Shrinivas18@users.noreply.github.com>
2023-10-26 21:52:20 +0530

    Add files via upload

---
+++ JAVASCRIPT LEVEL ONE/My_Script_Part-1(a).js
@@ -0,0 +1,10 @@
+//Alert has been set
+alert("Welcome to your bank");
+
+//variable deposit to store some prompt value.
+
+var deposit=prompt("How much would do you like to deposit today? ");
+alert("You have deposited : "+deposit);
+
+//Printing in console
+console.log("You are a cool person!");
