550e75a7e76170b27abf93eebe7d9c0424e9707b
Shrinivas Jamvant <119414094+Shrinivas18@users.noreply.github.com>
2023-10-26 21:52:31 +0530

    Add files via upload

---
+++ JAVASCRIPT LEVEL ONE/My_Script_Part-1(b).js
@@ -0,0 +1,4 @@
+var weight_in_lbs=prompt("What is the weight in pounds:")
+var weight_in_kgs=weight_in_lbs*0.454
+alert("The weight in kilograms is: "+weight_in_kgs+" Kilograms")
+console.log("Conversion Completed")
